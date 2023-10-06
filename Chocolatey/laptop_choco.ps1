25332df84425074ba4da168acc3f8dc6aaecead1
Leon Vinteanu <lvinteanu@lenovo.com>
2023-10-06 19:49:14 +0300

    updated with zsh, p10k, neofetch configs

--- Chocolatey/laptop_choco.ps1
+++
@@ -1,8 +0,0 @@
-choco install authy-desktop --version 2.2.1 -y
-choco install microsoft-windows-terminal --version 1.15.2524.0 -y
-choco install discord --version 1.0.9005 -y
-choco install pycharm-community --version 2022.2.3 -y
-choco install vscode --version 1.72.2 -y
-choco install spotify --version 1.1.96.785 -y
-choco install 7zip --version 22.1 -y
-choco install obsidian --version 1.0.0 -y
