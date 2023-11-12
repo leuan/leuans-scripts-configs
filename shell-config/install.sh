65d1fdb8e19de8bfce6064dedad0f6b147d6c8ba
Leon Vinteanu <lvinteanu@lenovo.com>
2023-11-12 02:04:13 +0200

    removed oh my zsh

--- shell-config/install.sh
+++ shell-config/install.sh
@@ -14,15 +14,14 @@ echo \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt install zsh neofetch lolcat exa nvm python3 ghdl gtkwave gh docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin podman -y
+sudo apt install zsh unzip neofetch lolcat exa python3 ghdl gtkwave gh docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin podman -y
 sudo groupadd docker
 sudo usermod -aG docker $USER
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 curl -fsSL https://bun.sh/install | bash
 nvm install node
-
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $HOME/.oh-my-zsh/plugins/zsh-autocomplete
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 echo "Moving configs..."
 cp -f .zshrc $HOME/
